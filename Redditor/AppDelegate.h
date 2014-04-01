c85a3018ee31a565bca81ab5dddef1029dad7e04
eddielau <eddielau224@gmail.com>
2014-04-01 01:42:25 -0700

    Initialized project

---
+++ Redditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Redditor
+//
+//  Created by Eddie Lau on 4/1/14.
+//  Copyright (c) 2014 Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Redditor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Redditor
+//
+//  Created by Eddie Lau on 4/1/14.
+//  Copyright (c) 2014 Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
