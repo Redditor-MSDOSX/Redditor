c589f443c97b3a8735b2a3a0956ccc717e1ffeba
qkw24 <qkw2006@gmail.com>
2014-04-18 01:25:41 -0700

    UI V1

---
+++ MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  Redditor
+//
+//  Created by Kevin Qi on 4/18/14.
+//  Copyright (c) 2014 Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
